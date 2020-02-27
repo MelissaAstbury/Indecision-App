e95e0c26c9fbfe21f363f6d99479ab8d6b220396
Melissa Astbury <melissajastbury@gmail.com>
2020-02-27 20:50:30 +0000

    ternary expressions and conditonal rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,52 @@
 "use strict";
 
-console.log("app.js is running");
+// console.log("app.js is running");
 
 //JSX - Jacascript XML. This is a language extension of Javascript
+var title = "Indecision App";
+var subTitle = "This is some information";
+var listItems = ["Item one", "Item two"];
+var todos = 6;
+
+function getTitle(title) {
+    if (title) {
+        return React.createElement(
+            "h1",
+            null,
+            title
+        );
+    }
+}
 
 var template = React.createElement(
-  "p",
-  null,
-  "This is JSX from app.js!"
+    "div",
+    null,
+    getTitle(title),
+    React.createElement(
+        "p",
+        null,
+        subTitle
+    ),
+    subTitle ? 'subTitle exists' : 'I dont exist',
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            listItems[0]
+        ),
+        React.createElement(
+            "li",
+            null,
+            listItems[1]
+        )
+    ),
+    todos && todos >= 6 && React.createElement(
+        "p",
+        null,
+        "You are too young"
+    )
 );
 
 var appRoot = document.getElementById("app");
--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
-console.log("app.js is running");
+// console.log("app.js is running");
 
 //JSX - Jacascript XML. This is a language extension of Javascript
+let title = "Indecision App";
+let subTitle = "This is some information";
+let listItems = ["Item one", "Item two"];
+let todos = 6;
 
-let template = <p>This is JSX from app.js!</p>
+function getTitle(title) {
+    if (title) {
+        return <h1>{title}</h1>;
+    }
+}
+
+
+let template = (
+    <div>
+        {getTitle(title)}
+        <p>{subTitle}</p>
+        {subTitle ? 'subTitle exists' : 'I dont exist'}
+        <ol>
+            <li>{listItems[0]}</li>
+            <li>{listItems[1]}</li>
+        </ol>
+        {(todos && todos >= 6) && <p>You are too young</p>}
+    </div>
+);
+
 
 let appRoot = document.getElementById("app");
 
