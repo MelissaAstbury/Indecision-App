fc29d56514243cfefa62463c085adecd23e82564
Melissa Astbury <melissajastbury@gmail.com>
2020-03-02 20:06:17 +0000

    arrow function challenge - introduced to map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,38 @@
 "use strict";
 
-var getFirstName = function getFirstName(name) {
-  return name.split(" ")[0];
+//arguments object - no longer bound with arrow functions
+
+var add = function add(a, b) {
+    return a + b;
 };
+console.log(add(55, 1));
 
-// const getFirstName = (name) => {
-//     return name.split(' ')[0];
-// }
+// this keyword - no longer bound
 
-console.log(getFirstName('Melissa Astbury'));
+var user = {
+    name: "Melissa",
+    cities: ["Manchester", "America", "Wales"],
+    printPlacedLived: function printPlacedLived() {
+        var _this = this;
+
+        this.cities.forEach(function (city) {
+            console.log(_this.name + " has lived in " + city);
+        });
+    }
+};
+user.printPlacedLived();
+
+//Challenge
+
+var multiplier = {
+    numbers: [10, 20, 30],
+    multiplyBy: 3,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
+};
+console.log(multiplier.multiply());
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -17,3 +17,14 @@ const user = {
     }
 };
 user.printPlacedLived();
+
+//Challenge
+
+const multiplier = {
+    numbers: [10, 20, 30],
+    multiplyBy: 3,
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy);
+    }
+};
+console.log(multiplier.multiply());
